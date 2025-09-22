ae9c3514063fcf31eaa3e048fed9f16663fdbf8f
Pedro Calderón <pedrocalderonn52@gmail.com>
2025-09-22 14:29:35 -0300

    removi a parte da lista desse lugar e iniciei uma variável clock, p contar globalmente o tempo

--- operacionalito/main.py
+++ operacionalito/main.py
@@ -1,23 +1,18 @@
 from models.processo import Processo
 
+clock: int = 0 # conta o tempo global de execução
+
 def get_processos():
     with open("operacionalito/dados.txt", "r") as file:
         lines = [line.rstrip() for line in file]
-        processos = [] # substituir pela estrutura de dados a ser criada
+        processos = [] # substituir pela estrutura de dados a ser criada (fila)
         for line in lines:
             pid, tempo_entrada, tempo_cpu, tempo_IO, prioridade = line.split(";") # divide as informações da linha com base no ';'
             processos.append(Processo(pid, tempo_entrada, tempo_cpu, tempo_IO, prioridade))
         return processos
 
 
-print(get_processos())
+processos = get_processos()
+print(processos)
 
-class Node():
-    def __init__(self, valor):
-        self.valor = valor
-        self.prox = None
-
-
-class Deque():
-    ...
 
